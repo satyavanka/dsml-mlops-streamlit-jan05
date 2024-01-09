55ec9086dd4a58f9bb2921c2d76772aee715714d
satyavanka <satyavmca@gmail.com>
2024-01-09 22:01:51 +0530

    Update stock_market.py
    Adding Streamlit code

--- stock_market.py
+++ stock_market.py
@@ -1,3 +1,61 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import yfinance as yf
+
+#history = msft.history(period="1mo")
+
+
+st.title("Stock Market App")
+st.write("This is the day where I have decide to rock")
+
+ticker_input  = st.text_input('Please enter ticker symbol', 'AAPL')
+
+
+ticker_data = yf.Ticker(ticker_input)
+
+start_date = st.date_input("Enter Starting date:",value=pd.to_datetime("2021-01-01"))
+End_date = st.date_input("Enter Ending date:",value=pd.to_datetime("today"))
+
+hist = ticker_data.history(start=start_date,end=End_date)
+
+st.write("APPLE stock data")
+
+# st.write(hist)
+st.dataframe(hist)
+
+# st.write("This plot is volume of the stock")
+# st.line_chart(hist.Volume)
+
+
+# st.write("This plot is closing price  of the stock")
+# st.line_chart(hist.Close)
+
+# import streamlit as st
+
+# col1, col2, col3 = st.columns(3)
+
+# with col1:
+#    st.header("A cat")
+#    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+# with col2:
+#    st.header("A dog")
+#    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+# with col3:
+#    st.header("An owl")
+#    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+# st.image('sunrise.jpg', caption='Sunrise by the mountains')
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.write("This plot is volume of the stock")
+    st.line_chart(hist.Volume)
+
+with col2:
+    st.write("This plot is closing price  of the stock")
+    st.line_chart(hist.Close)
+
